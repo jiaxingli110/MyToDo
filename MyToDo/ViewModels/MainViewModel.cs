efb009ffe18a67a61a2394bcc2dc70a2e0f9323f
jiaxing.che <471075887@qq.com>
2024-01-19 21:29:52 +0800

    添加项目文件。

---
+++ MyToDo/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using MyToDo.Comman;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyToDo.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        public MainViewModel()
+        {
+            MenuBars = new ObservableCollection<MenuBar>();
+            CreateMenuBar();
+        }
+        private ObservableCollection<MenuBar> menuBars;
+
+        public ObservableCollection<MenuBar> MenuBars
+        {
+            get { return menuBars; }
+            set { menuBars = value; RaisePropertyChanged(); }
+        }
+        void CreateMenuBar()
+        {
+             MenuBars.Add(new MenuBar() { Icon="Home",Title="首页",NameSpace="IndexView"});
+             MenuBars.Add(new MenuBar() { Icon="NotebookOutline",Title="待办事项",NameSpace="ToDoView"});
+             MenuBars.Add(new MenuBar() { Icon="NotebookPlus",Title="备忘录",NameSpace="MemoView"});
+             MenuBars.Add(new MenuBar() { Icon="Cog",Title="设置",NameSpace="SettingsView"});
+        }
+
+    }
+}
