3b5facd9bf407331a018dffa2501eae915ccfe6c
jiaxing.che <471075887@qq.com>
2024-01-23 15:11:10 +0800

    1、增加待办事项页面 2、增加备忘录页面

--- MyToDo/ViewModels/IndexViewModel.cs
+++ MyToDo/ViewModels/IndexViewModel.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using MyToDo.Comman;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyToDo.ViewModels
 {
-    internal class IndexViewModel
+    public class IndexViewModel :BindableBase
     {
+        public IndexViewModel()
+        {
+            TaskBars = new ObservableCollection<TaskBar>();
+            CreateTaskBars();
+        }
+        private ObservableCollection<TaskBar> taskBars;
+        public ObservableCollection<TaskBar> TaskBars
+        {
+            get { return taskBars; }
+            set { taskBars = value; RaisePropertyChanged();}
+        }
+        void CreateTaskBars()
+        {
+             TaskBars.Add(new TaskBar() { Icon="ClockFast", Title="汇总",Content="9",Color="#FF0CA0FF", Target=""});
+             TaskBars.Add(new TaskBar() { Icon="ClockCheckOutline", Title="汇总",Content="39",Color="#FF1ECA3A", Target=""});
+             TaskBars.Add(new TaskBar() { Icon="ChartLineVariant", Title="汇总",Content="19",Color="#FF02C6DC", Target=""});
+             TaskBars.Add(new TaskBar() { Icon="PlaylistStar", Title="汇总",Content="29",Color="#FFFFA000", Target=""});
+        }
     }
 }
--- MyToDo/Views/IndexView.xaml.cs
+++ MyToDo/Views/IndexView.xaml.cs
@@ -18,7 +18,7 @@ namespace MyToDo.Views
     /// <summary>
     /// IndexView.xaml 的交互逻辑
     /// </summary>
-    public partial class IndexView : Page
+    public partial class IndexView : UserControl
     {
         public IndexView()
         {
